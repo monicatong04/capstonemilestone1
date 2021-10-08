34a9bddec398b5e9e726a69e233e4d2d00e052a3
Monica Tong <monicatongg@gmail.com>
2021-10-08 11:05:47 -0700

    first commit

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+$(function () {
+    var includes = $('[data-include]')
+    $.each(includes, function () {
+      var file = 'views/' + $(this).data('include') + '.html'
+      $(this).load(file)
+    })
+
+    document.title = "your site name"
+
+    $('[data-toggle="tooltip"]').tooltip()
+                                  
+  })
+
+
+
+ 
